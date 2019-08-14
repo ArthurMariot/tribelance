ff765a75a6d786376bf5f06f5ac1f05e993030e9
Ramzi SAIDANI <ramzi@ramzi.fr>
2019-08-14 16:03:53 -0700

    uptodate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  # after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
@@ -27,3 +26,5 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
+
+
