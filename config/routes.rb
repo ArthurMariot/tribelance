f23221de37e6659bb8ace7ab224fb4cdab17165d
edouard uguen <uguen.edouard@gmail.com>
2019-08-14 15:48:37 +0200

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :candidatures, only: [:index, :new, :create, :show, :destroy]
     :details_candidatures
   end
-  get 'missions/:id/details_candidatures', to: 'missions#details_candidatures'
+  get 'missions/:id/details_candidatures', to: 'missions#details_candidatures', as: :details_candidatures
   get 'dashboard', to: 'missions#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
