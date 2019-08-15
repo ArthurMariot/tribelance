ed191cef79ef75f03465e98425cffe84548ab0e2
Ramzi SAIDANI <ramzi@ramzi.fr>
2019-08-15 12:52:56 -0700

    new labels for forms, regexp validation for linkedin url, mandatory picture for avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_184709) do
+ActiveRecord::Schema.define(version: 2019_08_15_175509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_184709) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar"
+    t.string "linkedinurl"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
