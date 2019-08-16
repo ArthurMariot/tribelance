fbc1a5a29478b72b9339895a2fa599a430713dff
Louis UGUEN <louisuguen1@gmail.com>
2019-08-16 11:48:24 +0200

    font test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.serve_static_assets = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
@@ -92,3 +94,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
